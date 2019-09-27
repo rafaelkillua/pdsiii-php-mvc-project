8902c914ab9aacc6eb6244af096cd638e9ce7347
Rafael Sampaio Tavares <rafael_killua@msn.com>
2019-09-27 16:34:13 -0300

    fix nome de variável

--- app/views/v_detalhar_produto.php
+++ app/views/v_detalhar_produto.php
@@ -7,13 +7,14 @@
   <body>
     <div class="col-12 d-flex flex-column align-items-center">
       <?php
-        if (!empty($dados["produto"])) { ?>
-          <p><strong>ID Produto: </strong><?=$dados["produto"]->getProduto()->getId()?></p>
-          <p><strong>Nome: </strong><?=$dados["produto"]->getProduto()->getNome()?></p>
-          <p><strong>Preço de compra: </strong><?=$dados["produto"]->getPrecoCompra()?></p>
-          <p><strong>Preço de venda: </strong><?=$dados["produto"]->getPrecoVenda()?></p>
-          <p><strong>Quantidade: </strong><?=$dados["produto"]->getQuantidade()?></p>
-          <p><strong>Status: </strong><?=$dados["produto"]->getStatus() == 1 ? "Ativo" : "Inativo"?></p>
+        if (!empty($dados["produto"])) {
+          $precoProduto = $dados["produto"]; ?>
+          <p><strong>ID Produto: </strong><?=$precoProduto->getProduto()->getId()?></p>
+          <p><strong>Nome: </strong><?=$precoProduto->getProduto()->getNome()?></p>
+          <p><strong>Preço de compra: </strong><?=$precoProduto->getPrecoCompra()?></p>
+          <p><strong>Preço de venda: </strong><?=$precoProduto->getPrecoVenda()?></p>
+          <p><strong>Quantidade: </strong><?=$precoProduto->getQuantidade()?></p>
+          <p><strong>Status: </strong><?=$precoProduto->getStatus() == 1 ? "Ativo" : "Inativo"?></p>
         <?php }
       ?>
       <a href="<?=BASE_URL."/produtos"?>" class="btn btn-primary">Voltar</a>
