607610c3072c95d728a3a03f64aadcbfb079fb01
Rafael Sampaio Tavares <rafael_killua@msn.com>
2019-09-30 12:48:20 -0300

    att pra db online (db4free)

--- app/config/banco.php
+++ app/config/banco.php
@@ -1,5 +1,5 @@
 <?php
 require_once PATH_APP . "/models/DAO/Conexao.php";
 
-$conexaoBanco = Conexao::getInstancia("localhost", "projeto-mvc", "root", "");
+$conexaoBanco = Conexao::getInstancia("db4free.net", "grafica", "rafaelkillua", "v2GH26pX4gdg9hL");
 $conexaoBanco = $conexaoBanco->getConexao();
